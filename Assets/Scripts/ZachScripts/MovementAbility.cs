275dbed542b1a70137fcd2a5853fa51600be379b
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-12-13 17:11:00 -0600

    moved my scripts into their own folder

---
+++ Assets/Scripts/ZachScripts/MovementAbility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZachScripts
+{
+    public class MovementAbility : Ability
+    {
+        float speed;
+        float speedOverTime;
+    }
+}
---
+++ Assets/Scripts/ZachScripts/ShieldAbility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZachScripts
+{
+    public class ShieldAbility : Ability
+    {
+        float health;
+        float size;
+        float duration;
+    }
+}
