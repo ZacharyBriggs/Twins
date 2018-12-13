41449130ea73da42296af753fc0e0499d5a08f4c
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-12-13 17:10:28 -0600

    behaviour for the projectile the turret fires

---
+++ Assets/Scripts/ZachScripts/BulletBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : Projectile
+{
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        this.transform.position += this.transform.forward * speed;
+        lifetime -= Time.deltaTime;
+        if(lifetime <= 0)
+        {
+            Expire();
+        }
+	}
+    public override void Expire()
+    {
+        Destroy(this.gameObject);
+    }
+}
