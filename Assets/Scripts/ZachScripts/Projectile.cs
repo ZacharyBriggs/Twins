d640a3e282d111aa836b68669b636fa1f41b3a10
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-12-13 17:09:53 -0600

    created a turret that shoots projectiles in a straight line

---
+++ Assets/Scripts/ZachScripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Luke;
+
+public abstract class Projectile : MonoBehaviour
+{
+    public float damage;
+    public float speed;
+    public float lifetime;
+    public abstract void Expire();
+    public virtual void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            var player = collision.gameObject.GetComponent<CharacterBehaviour>();
+            player.TakeDamage(damage);
+            Debug.Log("hit player");
+            Destroy(this.gameObject);
+        }
+        else if(collision.gameObject.tag != "Enemy")
+        {
+            Debug.Log("hit a wall");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ZachScripts/TurretBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBehaviour : MonoBehaviour
+{
+    public float RoF; //Rate of fire
+    private float timer;
+    public Projectile projectileprefab;
+	// Use this for initialization
+	void Start ()
+    {
+        timer = RoF;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            var proj = Instantiate<Projectile>(projectileprefab);
+            proj.transform.position = this.transform.position;
+            proj.transform.forward = this.transform.forward;
+            timer = RoF;
+        }
+	}
+}
