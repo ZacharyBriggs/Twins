2146d5b6841c47d518a0f77b2fcc2f0c6ea30d0a
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-12-14 13:03:22 -0600

    tried fixing the spike floors

--- Assets/Scripts/ZachScripts/SpikeBehaviour.cs
+++ Assets/Scripts/ZachScripts/SpikeBehaviour.cs
@@ -5,33 +5,35 @@ using Luke;
 
 namespace ZachScripts
 {
-    public class FloorHazardBehaviour : MonoBehaviour
+    public class SpikeBehaviour : MonoBehaviour
     {
         public float damage;
         public float RoD; //rate of damage
-        private float timer;
+        public float timer;
         public bool damageEverything;
         private void Start()
         {
-            RoD = timer;
+            timer = RoD; ;
         }
         private void Update()
         {
             timer -= Time.deltaTime;
-            if(timer == 0)
+            if(timer <= 0)
             {
                 timer = RoD;
+                Debug.Log("Spikes Out");
             }
         }
 
-        private void OnCollisionStay(Collision collision)
+        private void OnTriggerStay(Collider other)
         {
-            var character = collision.gameObject.GetComponent<CharacterBehaviour>();
-            if (character.gameObject.tag == "Player" && timer == 0)
+            var character = other.gameObject.GetComponent<CharacterBehaviour>();
+            if (character.gameObject.tag == "Player" &&  timer <= 0)
             {
                 character.TakeDamage(damage);
+                Debug.Log("Damaged Player");
             }
-            else if(character.gameObject.tag == "Enemy" && damageEverything && timer == 0)
+            else if (character.gameObject.tag == "Enemy" && damageEverything && timer <= 0)
             {
                 character.TakeDamage(damage);
             }
